121316e6ef611d63ee13838e2c375f9c0240bc81
PedroMarioaBros <pedromariocaldas@gmail.com>
2026-09-13 05:23:41 -0300

    xex: expose native render-driver audio adapter

---
+++ src/platform/xex/audio_xex.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int smb360_xex_audio_init(void);
+void smb360_xex_audio_shutdown(void);
+int smb360_xex_audio_ready(void);
+
+#ifdef __cplusplus
+}
+#endif
