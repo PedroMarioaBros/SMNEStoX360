2efde3458c51799fa7a0b5fc371bd6d7f5aa3bd2
PedroMarioaBros <pedromariocaldas@gmail.com>
2026-09-13 05:20:42 -0300

    xex: expose framebuffer presenter C API

---
+++ src/platform/xex/video_fb.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct xex_fb {
+    volatile uint32_t *pixels;
+    uint32_t width;
+    uint32_t height;
+    uint32_t tiled_width;
+    uint32_t format_10bit;
+};
+
+int smb360_xex_fb_open(struct xex_fb *out);
+int smb360_xex_fb_present_rgb888(struct xex_fb *fb,
+                                 const uint8_t *rgb,
+                                 uint32_t src_w,
+                                 uint32_t src_h);
+
+#ifdef __cplusplus
+}
+#endif
