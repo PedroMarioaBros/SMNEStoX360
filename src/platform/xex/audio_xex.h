7fb98358c0ef74c4a3e65f11ea0b88934df0563c
PedroMarioaBros <pedromariocaldas@gmail.com>
2026-09-13 05:37:16 -0300

    xex: make APU audio production single-threaded

--- src/platform/xex/audio_xex.h
+++ src/platform/xex/audio_xex.h
@@ -7,6 +7,8 @@ extern "C" {
 int smb360_xex_audio_init(void);
 void smb360_xex_audio_shutdown(void);
 int smb360_xex_audio_ready(void);
+/* Call once per 60 Hz emulation frame, after apu_step_frame(). */
+void smb360_xex_audio_produce_frame(void);
 
 #ifdef __cplusplus
 }
